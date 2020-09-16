54d44cc4292589d3414648bf631f29d561d66b11
kotran222 <kobetrankhoi222@gmail.com>
2020-09-15 23:41:28 -0700

    please work

--- puppystore.js
+++ puppystore.js
@@ -9,13 +9,3 @@ function closeSlideMenu(){
     document.getElementById('side-menu').style.width = '0';
     document.getElementById('main').style.marginRight = '0';
 }
-
-// Place Holder for navigation bar
-$(function(){
-    $("#nav-placeholder").load("sub_tabs/navigation.html");
-  });
-
-//Place holder for footer
-$(function(){
-    $("#footer-placeholder").load("sub_tabs/footer.html");
-  });
